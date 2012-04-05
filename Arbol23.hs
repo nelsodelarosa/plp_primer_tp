18e4f11122484dbe156109bbcd6b455e66603191
martin <tolacelave@gmail.com>
2012-04-05 13:26:03 -0300

    A con tilde

--- Arbol23.hs
+++ Arbol23.hs
@@ -1,13 +1,13 @@
-module Árbol23 where
+module Arbol23 where
 
-data Árbol23 a b = Hoja a | Dos b (Árbol23 a b) (Árbol23 a b) | Tres b b (Árbol23 a b) (Árbol23 a b) (Árbol23 a b)
+data Arbol23 a b = Hoja a | Dos b (Arbol23 a b) (Arbol23 a b) | Tres b b (Arbol23 a b) (Arbol23 a b) (Arbol23 a b)
 
-{- Funciones para mostrar el árbol. -}
+{- Funciones para mostrar el Arbol. -}
 
-instance (Show a, Show b) => Show (Árbol23 a b) where
+instance (Show a, Show b) => Show (Arbol23 a b) where
     show x = concatMap (++"\n") (padTree 0 x)
 
-padTree:: (Show a, Show b) => Int -> (Árbol23 a b)-> [String]
+padTree:: (Show a, Show b) => Int -> (Arbol23 a b)-> [String]
 padTree i (Hoja x) = [(pad i) ++  (show x)]
 padTree i (Dos x a1 a2) = [(pad i) ++  (show x) ] ++ (concatMap (padTree (i + 4)) [a1,a2])
 padTree i (Tres x y a1 a2 a3) = [(pad i) ++  (show x) ++ (' ':(show y))] ++ (concatMap (padTree (i + 4)) [a1,a2,a3])
@@ -19,41 +19,41 @@ pad i = replicate i ' '
 
 --foldA23::
 
---Lista en preorden de los internos del árbol.
---internos::Árbol23 a b->[b]
+--Lista en preorden de los internos del Arbol.
+--internos::Arbol23 a b->[b]
 
 --Lista las hojas de izquierda a derecha.
---hojas::Árbol23 a b->[a]
+--hojas::Arbol23 a b->[a]
 
---esHoja::Árbol23 a b->Bool
+--esHoja::Arbol23 a b->Bool
 
---mapA23::(a->c)->(b->d)->Árbol23 a b->Árbol23 c d
+--mapA23::(a->c)->(b->d)->Arbol23 a b->Arbol23 c d
 
 --Ejemplo de uso de mapA23.
 --Incrementa en 1 el valor de las hojas.
-incrementarHojas::Num a =>Árbol23 a b->Árbol23 a b
+incrementarHojas::Num a =>Arbol23 a b->Arbol23 a b
 incrementarHojas = mapA23 (+1) id
 
 
---Trunca el árbol hasta un determinado nivel. Cuando llega a 0, reemplaza el resto del árbol por una hoja con el valor indicado.
---Funciona para árboles infinitos.
---truncar::a->Integer->Árbol23 a b->Árbol23 a b
+--Trunca el Arbol hasta un determinado nivel. Cuando llega a 0, reemplaza el resto del Arbol por una hoja con el valor indicado.
+--Funciona para Arboles infinitos.
+--truncar::a->Integer->Arbol23 a b->Arbol23 a b
 
 --Evalúa las funciones tomando los valores de los hijos como argumentos.
 --En el caso de que haya 3 hijos, asocia a izquierda.
---evaluar::Árbol23 a (a->a->a)->a
+--evaluar::Arbol23 a (a->a->a)->a
 
-{- Árboles de ejemplo. -}
-arbolito1::Árbol23 Char Int
+{- Arboles de ejemplo. -}
+arbolito1::Arbol23 Char Int
 arbolito1 = Tres 0 1
 	      (Dos 2 (Hoja 'a') (Hoja 'b'))
 	      (Tres 3 4 (Hoja 'c') (Hoja 'd') (Dos 5 (Hoja 'e') (Hoja 'f')))
 	      (Dos 6 (Hoja 'g') (Dos 7 (Hoja 'h') (Hoja 'i')))
 
-arbolito2::Árbol23 Int Bool
+arbolito2::Arbol23 Int Bool
 arbolito2 = Dos True (Hoja (-1)) (Tres False True (Hoja 0) (Hoja (-2)) (Hoja 4))
 
-arbolito3::Árbol23 Int (Int->Int->Int)
+arbolito3::Arbol23 Int (Int->Int->Int)
 arbolito3 = Dos (+) (Tres (*) (-) (Hoja 1) (Hoja 2) (Hoja 3)) (incrementarHojas arbolito3)
 
 --Ejemplos:
--- Diccionario.hs
+++ Diccionario.hs
@@ -2,17 +2,17 @@ module Diccionario (Diccionario, vacío, definir, definirVarias, obtener, claves
 
 import Maybe
 import List
-import Árbol23
+import Arbol23
 
 {- Definiciones de tipos. -}
 
 type Comp clave = clave->clave->Bool
-type Estr clave valor = Árbol23 (clave,valor) clave
+type Estr clave valor = Arbol23 (clave,valor) clave
 
 data Diccionario clave valor = Dicc {cmp :: Comp clave, estructura :: Maybe (Estr clave valor)}
 --El comparador es por menor.
 
-{- Funciones provistas por la cátedra. -}
+{- Funciones provistas por la cAtedra. -}
 
 --Inserta un nuevo par clave, valor en una estructura que ya tiene al menos un dato.
 insertar::clave->valor->Comp clave->Estr clave valor-> Estr clave valor
@@ -25,11 +25,11 @@ interceptar (x,y) f1 f2 = case y of
 			       Just z -> f2 x z
 
 {- Inserta una clave con su valor correspondiente. Si se actualiza el índice, el cambio se propaga hacia arriba
-   para mantener balanceado el árbol.
+   para mantener balanceado el Arbol.
    Usamos recursión explícita porque este tipo de recursión no es estructural (no se aplica a todos los hijos). -}
 insertarYPropagar::clave->valor->Comp clave->Estr clave valor-> (Estr clave valor, Maybe (clave, Estr clave valor))
 insertarYPropagar c v comp a23 = let rec = insertarYPropagar c v comp in case a23 of
-	--Si es hoja, elegimos la máxima de las claves y propagamos el balanceo hacia arriba.
+	--Si es hoja, elegimos la mAxima de las claves y propagamos el balanceo hacia arriba.
 	Hoja (ch,vh) -> if comp c ch 
 				then (Hoja (c,v), Just (ch, Hoja (ch,vh)))
 				else (Hoja (ch, vh), Just (c, Hoja (c,v)))
@@ -47,21 +47,21 @@ insertarYPropagar c v comp a23 = let rec = insertarYPropagar c v comp in case a2
 						(\s1 -> Dos c1 a1 s1)
 						(\s1 (c3, s2) -> Tres c1 c3 a1 s1 s2), Nothing)
 	{- Nodo-tres sólo propaga si de abajo propagan, los tres casos son muy similares
-	   Sólo cambia en que árbol se inserta. -}
+	   Sólo cambia en que Arbol se inserta. -}
 	Tres c1 c2 a1 a2 a3 -> if comp c c1
 				then 
-					-- La clave debe ir en el primer árbol.
+					-- La clave debe ir en el primer Arbol.
 					interceptar (rec a1) 
 						(\s1 -> (Tres c1 c2 s1 a2 a3, Nothing))
 						(\s1 (c3, s2) -> (Dos c3 s1 s2, Just(c1, Dos c2 a2 a3)))
 				else if comp c c2
 				then 
-					-- La clave debe ir en el árbol del medio.
+					-- La clave debe ir en el Arbol del medio.
 					interceptar (rec a2) 
 						(\s1 -> (Tres c1 c2 a1 s1 a3, Nothing))
 						(\s1 (c3, s2) -> (Dos c1 a1 s1, Just(c3, Dos c2 s2 a3)))
 				else 
-					--La clave debe ir en el último árbol.
+					--La clave debe ir en el último Arbol.
 					interceptar (rec a3) 
 						(\s1 -> (Tres c1 c2 a1 a2 s1, Nothing))
 						(\s1 (c3, s2) -> (Dos c1 a1 a2, Just(c2, Dos c3 s1 s2)))
