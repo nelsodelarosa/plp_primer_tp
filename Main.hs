088543ecad83a4ac00ad3b8af3db2cb1f7395cd3
Nelson De La Rosa <tltps2012@hotmail.com>
2012-04-06 15:12:41 -0300

    primer resulucion de la busqueda

--- Main.hs
+++ Main.hs
@@ -5,17 +5,18 @@ import Maybe
 
 {- Función a implementar. -}
 
---búsquedaDelTesoro::Eq a=>a->(a->Bool)->Diccionario a a->Maybe a 
+busquedaDelTesoro::Eq a=>a->(a->Bool)->Diccionario a a->Maybe a
+busquedaDelTesoro indicio check d = foldr (\x y->if isNothing x then Nothing else if check (fromJust x) then x else y) Nothing (iterate (\x->case x of {(Just c)->obtener c d; Nothing -> Nothing}) (Just indicio))
 
 {- Diccionarios de prueba: -}
 
 dicc1::Diccionario Int String
-dicc1 = definirVarias [(0,"Hola"),(-10,"Chau"),(15,"Felicidades"),(2,"etc."),(9,"a")] (vacío (<))
+dicc1 = definirVarias [(0,"Hola"),(-10,"Chau"),(15,"Felicidades"),(2,"etc."),(9,"a")] (vacio (<))
 
 dicc2::Diccionario String String
-dicc2 = definirVarias [("inicio","casa"),("auto","flores"),("calle","auto"),("casa","escalera"),("ropero","alfajor"),("escalera","ropero")] (vacío (<))
+dicc2 = definirVarias [("inicio","casa"),("auto","flores"),("calle","auto"),("casa","escalera"),("ropero","alfajor"),("escalera","ropero")] (vacio (<))
 
 dicc3::Diccionario Int String
-dicc3 = definirVarias [(0,"Hola"),(-10,"Chau"),(15,"Felicidades"),(2,"etc."),(9,"a")] (vacío (\x y->x `mod` 5 < y `mod` 5))
+dicc3 = definirVarias [(0,"Hola"),(-10,"Chau"),(15,"Felicidades"),(2,"etc."),(9,"a")] (vacio (\x y->x `mod` 5 < y `mod` 5))
 
 --Ejemplo: búsquedaDelTesoro "inicio" ((=='a').head) dicc2 = Just "alfajor"
